1ca22117699695e40afcb95425c77013ad9e9015
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-06-23 17:06:00 +0530

    import getUserProfileController and isAuth use  in router

--- Server/routes/userRoute.js
+++ Server/routes/userRoute.js
@@ -2,6 +2,8 @@ import express from "express";
 
 import { registerController } from "../controller/userController.js";
 import { loginController} from "../controller/userController.js";
+import{getUserProfileController}from "../controller/userController.js"
+import { isAuth } from "../middlewares/authMiddleware.js";
 
 // router object
 const router = express.Router();
@@ -10,7 +12,11 @@ const router = express.Router();
 // routers
 router.post("/register", registerController);
 
+// login
 router.post("/login",loginController)
+
+// profile
+router.get("/profile",isAuth, getUserProfileController)
 
 
 export default router;
