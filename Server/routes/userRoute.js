7be41e8478dd74af5711b6567991c443c1c059f3
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-07-08 15:29:21 +0530

    add logout route

--- Server/routes/userRoute.js
+++ Server/routes/userRoute.js
@@ -3,7 +3,9 @@ import express from "express";
 import { registerController } from "../controller/userController.js";
 import { loginController} from "../controller/userController.js";
 import{getUserProfileController}from "../controller/userController.js"
+import{logOutController} from "../controller/userController.js"
 import { isAuth } from "../middlewares/authMiddleware.js";
+
 
 // router object
 const router = express.Router();
@@ -17,6 +19,9 @@ router.post("/login",loginController)
 
 // profile
 router.get("/profile",isAuth, getUserProfileController)
+
+//logout
+router.post("/logout" ,logOutController )
 
 
 export default router;
